271a2a3370f4d0920d376b01dc8ecc67b58a8a88
khailq28 <khailuong61@gmail.com>
2021-09-23 18:20:17 +0700

    #210923: create SearchBar.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { SafeAreaView, View } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
     return (
         <SafeAreaView style={{ backgroundColor: "#eee" }}>
             <View style={{ backgroundColor: "white", padding: 15 }}>
                 <HeaderTabs />
+                <SearchBar />
             </View>
         </SafeAreaView>
     );
